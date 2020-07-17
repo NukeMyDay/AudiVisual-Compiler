cc6d21d2621c98f4743ce6327fbcadf1599a71d3
Florian zu Dreele <zetpdep@gmail.com>
2020-07-17 14:04:53 +0200

    Added second audio track to sample

--- src/Lexer.kt
+++ src/Lexer.kt
@@ -114,6 +114,10 @@ fun main () {
         \clef "treble" \numericTimeSignature\time 4/4 \tempo 4=40
         c2 d e <f g>2 g a4 a fes a a g1 a4 a a a \break g1 f4 f f f e2 e d4 d d d c1
     }
+    {
+        \clef "treble" \numericTimeSignature\time 4/4 \tempo 4=40
+        c2 d e <f g>2 g a4 a fes a a g1 a4 a a a \break g1 f4 f f f e2 e d4 d d d c1
+    }
     """.trimMargin()
 
     val lexer = Lexer(input)
